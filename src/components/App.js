c7c6b30eae91ec57f571a4eb471b8571d0b97cc3
04rookie <47923475+04rookie@users.noreply.github.com>
2021-02-10 18:05:15 +0530

    completed

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,15 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
-
+import notes from "./../notes.js";
 function App() {
+  var a = 3 + 3;
   return (
     <div>
       <Header />
-      <Note />
+      {notes.map((values) => (
+        <Note key={values.key} title={values.title} content={values.content} />
+      ))}
       <Footer />
     </div>
   );
